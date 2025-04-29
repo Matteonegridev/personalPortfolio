bfd58462316b0734ef44de7ea7e54eb315fbbea7
Matteo <matteonegridev@gmail.com>
2025-04-29 23:50:49 +0200

    menu effects

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import Logo from "./LogoColor.vue";
+import Logo from "../utils/LogoColor.vue";
+import LogoWhite from "../utils/LogoWhite.vue";
+import { easeIn, easeOut, motion } from "motion-v";
 
 const navLinks = ["About Me", "Projects", "Contact Me"];
 
@@ -9,13 +11,45 @@ const isMenuOpen = ref(false);
 const toggleMenu = () => {
   isMenuOpen.value = !isMenuOpen.value;
 };
+
+const container = {
+  hidden: { opacity: 0, y: -20 },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.6,
+    },
+  },
+};
+
+const navbar = {
+  hidden: {
+    x: "-100%",
+    transition: {
+      easeOut,
+      duration: 0.2,
+      delay: 0.4,
+    },
+  },
+  open: {
+    x: 0,
+    transition: {
+      easeIn,
+      duration: 0.5,
+    },
+  },
+};
 </script>
 
 <template>
   <header class="fixed top-0 right-0 left-0 z-50">
     <nav class="relative flex items-center justify-between p-8">
-      <div class="inline">
+      <div v-if="!isMenuOpen" class="inline">
         <Logo />
+      </div>
+      <div v-else class="z-50">
+        <LogoWhite />
       </div>
       <button
         class="z-50 flex flex-col gap-1 lg:hidden"
@@ -27,25 +61,30 @@ const toggleMenu = () => {
         <span class="h-1 w-8 bg-white"></span>
       </button>
     </nav>
-    <Transition
-      enter-active-class="duration-300 ease-out"
-      enter-from-class="-translate-x-full"
-      enter-to-class="translate-x-0"
-      leave-active-class="duration-200 ease-in"
-      leave-from-class="translate-x-0"
-      leave-to-class="-translate-x-full"
+
+    <motion.nav
+      :variants="navbar"
+      initial="hidden"
+      :animate="isMenuOpen ? 'open' : 'hidden'"
+      class="bg-primary absolute top-0 flex h-[100dvh] w-full items-center justify-center lg:hidden"
     >
-      <nav v-if="isMenuOpen" class="bg-primary absolute top-0 w-full lg:hidden">
-        <ul class="mt-22 flex flex-col gap-8 p-6">
-          <li
-            v-for="link in navLinks"
-            :key="link"
-            class="title-mobile-h4 w-full text-white"
-          >
-            <a href="#">{{ link }}</a>
-          </li>
-        </ul>
-      </nav>
-    </Transition>
+      <ul class="space-y-12 text-center">
+        <motion.li
+          initial="hidden"
+          :animate="isMenuOpen ? 'show' : 'hidden'"
+          :variants="container"
+          v-for="link in navLinks"
+          :key="link"
+          class="title-mobile-h2 w-full px-10 py-5 text-white"
+        >
+          <a href="#">{{ link }}</a>
+        </motion.li>
+
+        <div class="flex justify-around">
+          <p>socialink</p>
+          <p>socialink</p>
+        </div>
+      </ul>
+    </motion.nav>
   </header>
 </template>
