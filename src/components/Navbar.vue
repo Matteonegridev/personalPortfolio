b380f6459bd9f8ef10cefb53faf815937a5b921e
Matteo <matteonegridev@gmail.com>
2025-05-02 23:37:17 +0200

    fixed navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { ref, watchEffect } from "vue";
+import { ref } from "vue";
 import Logo from "../utils/LogoColor.vue";
-import LogoWhite from "../utils/LogoWhite.vue";
+
 import Button from "./Button.vue";
-import { easeIn, easeOut, motion, useTime, useTransform } from "motion-v";
+import { easeIn, motion, useTime, useTransform } from "motion-v";
 import clsx from "clsx";
 
 const navLinks = ["About Me", "Projects", "Contact Me"];
@@ -15,7 +15,6 @@ const rotateBg = useTransform(rotate, (r) => {
 });
 
 const isMenuOpen = ref(false);
-const isWindowScrolling = ref(false);
 
 const toggleMenu = () => {
   isMenuOpen.value = !isMenuOpen.value;
@@ -24,8 +23,8 @@ const toggleMenu = () => {
 const variantsUl = {
   open: {
     transition: {
-      staggerChildren: 0.1,
-      delayChildren: 0.2,
+      staggerChildren: 0.2,
+      delayChildren: 0.3,
     },
   },
   closed: {
@@ -44,114 +43,136 @@ const navbar = {
   },
   hidden: {
     x: "-100%",
-    transition: { duration: 0.4, ease: "easeIn" },
+    transition: { duration: 0.4, ease: "easeIn", delay: 0.5 },
   },
 };
 
-const variantsSocial = {
+const ideaVar = {
   open: {
     y: 0,
     opacity: 1,
     transition: {
-      duration: 0.2,
-      delay: 0.6,
+      duration: 0.5,
+      ease: easeIn,
+      delay: 0.8,
     },
   },
-  closed: {
-    y: -20,
+  hidden: {
+    y: -10,
     opacity: 0,
-    transition: {
-      duration: 0.2,
-      delay: 0,
-    },
   },
 };
 
-watchEffect(() => {
-  const handleScroll = () => {
-    isWindowScrolling.value = window.scrollY > 100;
-  };
-
-  window.addEventListener("scroll", handleScroll);
-
-  return () => window.removeEventListener("scroll", handleScroll);
-});
+const lineMotion = {
+  open: {
+    scaleX: 1,
+    transition: {
+      delay: 0.7,
+      duration: 0.3,
+      ease: easeIn,
+    },
+  },
+  hidden: {
+    scaleX: 0,
+    transition: {
+      duration: 0.3,
+    },
+  },
+};
 </script>
 
 <template>
-  <header class="fixed top-0 right-0 left-0 z-[1000]">
-    <nav
-      :class="
-        clsx('relative flex items-center justify-between px-10 py-6 lg:px-16', {
-          'backdrop-blur-sm transition duration-350 ease-in':
-            isWindowScrolling && !isMenuOpen,
-        })
-      "
+  <header
+    class="bg-dark/70 fixed top-0 right-0 left-0 z-50 flex items-center justify-around px-10 py-5 backdrop-blur-lg"
+  >
+    <div><Logo /></div>
+
+    <!-- Desktop -->
+    <ul
+      class="body-font-desktop flex items-center gap-10 font-bold max-lg:hidden"
     >
-      <div><Logo /></div>
-      <!-- Desktop -->
-      <ul
-        class="body-font-desktop flex items-center gap-10 font-bold max-lg:hidden"
+      <li
+        class="hover:text-secondary cursor-pointer text-white"
+        v-for="link of navLinks"
       >
-        <li
-          class="hover:text-secondary cursor-pointer text-white"
-          v-for="link of navLinks"
-        >
-          {{ link }}
-        </li>
-      </ul>
-      <div class="relative ml-auto max-lg:hidden">
-        <Button
-          variant="secondary"
-          label="Get In Touch"
-          class="hover:text-secondary bg-dark hover:border-secondary button-effect relative z-10 w-[12.5ch] cursor-pointer transition-all duration-250 ease-in hover:pr-10 hover:pl-5"
-        />
-        <motion.div
-          class="absolute -inset-[1px]"
-          :style="{ background: rotateBg, filter: 'blur(10px)' }"
-        />
-      </div>
-      <button
-        :class="clsx('z-[999] flex flex-col gap-1 lg:hidden')"
-        @click="toggleMenu"
-        aria-label="Toggle menu"
-      >
-        <span class="h-1 w-8 bg-white"></span>
-        <span class="h-1 w-8 bg-white"></span>
-        <span class="h-1 w-8 bg-white"></span>
-      </button>
-    </nav>
-    <!-- Mobile -->
+        <a :href="'#' + link"> {{ link }}</a>
+      </li>
+    </ul>
+    <div class="relative ml-auto max-lg:hidden">
+      <Button
+        variant="secondary"
+        label="Get In Touch"
+        class="hover:text-secondary bg-dark hover:border-secondary button-effect relative z-10 w-[12.5ch] cursor-pointer transition-all duration-250 ease-in hover:pr-10 hover:pl-5"
+      />
+      <motion.div
+        class="absolute -inset-[1px]"
+        :style="{ background: rotateBg, filter: 'blur(10px)' }"
+      />
+    </div>
+    <button
+      :class="clsx('z-[999] flex flex-col gap-1 lg:hidden')"
+      @click="toggleMenu"
+      aria-label="Toggle menu"
+    >
+      <span class="h-1 w-8 bg-white"></span>
+      <span class="h-1 w-8 bg-white"></span>
+      <span class="h-1 w-8 bg-white"></span>
+    </button>
     <motion.nav
       :variants="navbar"
       :initial="false"
       :animate="isMenuOpen ? 'open' : 'hidden'"
-      class="bg-primary absolute top-0 flex h-[100dvh] w-full flex-col justify-center gap-8 lg:hidden"
+      class="bg-primary absolute top-0 z-50 flex h-[100dvh] w-full flex-col items-center justify-center overflow-hidden px-6 lg:hidden"
     >
+      <!-- Glowing  -->
+      <div
+        class="absolute inset-0 -z-10 bg-gradient-to-br from-purple-600 via-indigo-500 to-pink-500 opacity-20 blur-2xl"
+      ></div>
+
+      <!-- Navigation Label -->
+      <h3 class="note-details-mobile mb-6 font-bold text-gray-400 uppercase">
+        Navigation
+      </h3>
+
+      <!-- Links -->
       <motion.ul
         :initial="false"
         :animate="isMenuOpen ? 'open' : 'closed'"
         :variants="variantsUl"
-        class="space-y-6 text-left"
+        class="mb-10 w-full space-y-6 text-center"
       >
         <motion.li
           :variants="variantsLi"
           v-for="link in navLinks"
           :key="link"
-          class="title-mobile-h2 w-full px-10 py-5 text-white"
+          class="title-mobile-h3 py-3 text-white"
         >
-          <a :href="'#' + link" @click="toggleMenu">{{ link }}</a>
+          <a :href="'#' + link" @click="toggleMenu" class="block">
+            {{ link }}
+          </a>
         </motion.li>
       </motion.ul>
       <motion.div
-        :variants="variantsSocial"
-        initial="closed"
-        :animate="isMenuOpen ? 'open' : 'closed'"
-        class="flex items-center justify-center gap-10"
+        :initial="false"
+        :variants="lineMotion"
+        :animate="isMenuOpen ? 'open' : 'hidden'"
+        class="bg-secondary mx-auto h-0.5 w-24 rounded-full"
+      />
+
+      <motion.div
+        :animate="isMenuOpen ? 'open' : 'hidden'"
+        :initial="false"
+        :variants="ideaVar"
+        class="mt-10 w-full"
       >
-        <p>socialink</p>
-        <p>socialink</p>
+        <p
+          class="note-details-mobile text-secondary mt-10 max-w-xs text-center"
+        >
+          Tell me about your idea and let's turn it into code!
+        </p>
       </motion.div>
     </motion.nav>
   </header>
+
+  <!-- Mobile -->
 </template>
