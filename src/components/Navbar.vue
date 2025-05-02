34b0c51db5da4132853a000887352edbdec9b2e8
Matteo <matteonegridev@gmail.com>
2025-05-02 22:15:43 +0200

    test

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -21,31 +21,48 @@ const toggleMenu = () => {
   isMenuOpen.value = !isMenuOpen.value;
 };
 
-const container = {
-  hidden: { opacity: 0, y: -20 },
-  show: {
-    opacity: 1,
-    y: 0,
+const variantsUl = {
+  open: {
     transition: {
-      delay: 0.6,
+      staggerChildren: 0.1,
+      delayChildren: 0.2,
     },
   },
+  closed: {
+    transition: { staggerChildren: 0.1, staggerDirection: -1 },
+  },
+};
+const variantsLi = {
+  open: { opacity: 1, x: 0, transition: { duration: 0.5 } },
+  closed: { opacity: 0, x: -20, transition: { duration: 0.5 } },
 };
 
 const navbar = {
-  hidden: {
-    x: "-110%",
-    transition: {
-      easeOut,
-      duration: 0.2,
-      delay: 0.4,
-    },
-  },
   open: {
     x: 0,
+    transition: { duration: 0.5, ease: "easeOut" },
+  },
+  hidden: {
+    x: "-100%",
+    transition: { duration: 0.4, ease: "easeIn" },
+  },
+};
+
+const variantsSocial = {
+  open: {
+    y: 0,
+    opacity: 1,
     transition: {
-      easeIn,
-      duration: 0.5,
+      duration: 0.2,
+      delay: 0.6,
+    },
+  },
+  closed: {
+    y: -20,
+    opacity: 0,
+    transition: {
+      duration: 0.2,
+      delay: 0,
     },
   },
 };
@@ -62,7 +79,7 @@ watchEffect(() => {
 </script>
 
 <template>
-  <header class="fixed top-0 right-0 left-0">
+  <header class="fixed top-0 right-0 left-0 z-[1000]">
     <nav
       :class="
         clsx('relative flex items-center justify-between px-10 py-6 lg:px-16', {
@@ -71,9 +88,7 @@ watchEffect(() => {
         })
       "
     >
-      <div :class="isMenuOpen ? 'z-50' : ''">
-        <component :is="isMenuOpen ? LogoWhite : Logo" />
-      </div>
+      <div><Logo /></div>
       <!-- Desktop -->
       <ul
         class="body-font-desktop flex items-center gap-10 font-bold max-lg:hidden"
@@ -97,7 +112,7 @@ watchEffect(() => {
         />
       </div>
       <button
-        :class="clsx('z-50 flex flex-col gap-1 lg:hidden')"
+        :class="clsx('z-[999] flex flex-col gap-1 lg:hidden')"
         @click="toggleMenu"
         aria-label="Toggle menu"
       >
@@ -109,27 +124,34 @@ watchEffect(() => {
     <!-- Mobile -->
     <motion.nav
       :variants="navbar"
-      initial="hidden"
+      :initial="false"
       :animate="isMenuOpen ? 'open' : 'hidden'"
-      class="bg-primary absolute top-0 flex h-[100dvh] w-full items-center justify-center lg:hidden"
+      class="bg-primary absolute top-0 flex h-[100dvh] w-full flex-col justify-center gap-8 lg:hidden"
     >
-      <ul class="space-y-12 text-center">
+      <motion.ul
+        :initial="false"
+        :animate="isMenuOpen ? 'open' : 'closed'"
+        :variants="variantsUl"
+        class="space-y-6 text-left"
+      >
         <motion.li
-          initial="hidden"
-          :animate="isMenuOpen ? 'show' : 'hidden'"
-          :variants="container"
+          :variants="variantsLi"
           v-for="link in navLinks"
           :key="link"
           class="title-mobile-h2 w-full px-10 py-5 text-white"
         >
           <a :href="'#' + link" @click="toggleMenu">{{ link }}</a>
         </motion.li>
-
-        <div class="flex justify-around">
-          <p>socialink</p>
-          <p>socialink</p>
-        </div>
-      </ul>
+      </motion.ul>
+      <motion.div
+        :variants="variantsSocial"
+        initial="closed"
+        :animate="isMenuOpen ? 'open' : 'closed'"
+        class="flex items-center justify-center gap-10"
+      >
+        <p>socialink</p>
+        <p>socialink</p>
+      </motion.div>
     </motion.nav>
   </header>
 </template>
