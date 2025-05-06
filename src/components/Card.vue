82974770c0b8b166f4f141f2821f141ab49a809f
Matteo <matteonegridev@gmail.com>
2025-05-06 11:58:12 +0200

    grid area

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,14 +3,16 @@ const props = defineProps(["image"]);
 </script>
 
 <template>
-  <section class="rounded-2xl border border-white">
-    <div>
+  <section class="grid place-content-center rounded-2xl border border-white">
+    <div class="grid-area-1-1">
       <picture>
         <img :src="props.image" alt="" class="rounded-2xl" />
       </picture>
     </div>
-    <div>
-      <h1 class="text-white">hello</h1>
+    <div class="grid-area-1-1">
+      <h1 class="text-white">Project:</h1>
+      <p class="text-white">Project Name</p>
+      <p class="text-white">Technologies used:</p>
     </div>
   </section>
 </template>
