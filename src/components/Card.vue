4cf4b159158c7b5be5cb0b2fafd5965e2733eb81
Matteo <matteonegridev@gmail.com>
2025-05-08 13:15:19 +0100

    adding icons on card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,16 +1,33 @@
 <script setup lang="ts">
-const props = defineProps(["image"]);
+const props = defineProps<{
+  image: string;
+  description: string;
+  title: string;
+  tech: string[];
+}>();
 </script>
 
 <template>
-  <div class="mt-5 grid border border-white">
-    <div class="grid-area">
-      <img :src="props.image" alt="" class="h-[20rem] w-full" />
+  <div
+    class="from-primary to-dark border-primary mt-5 h-auto cursor-pointer rounded-xl border-b-4 bg-linear-to-br to-30% p-2 transition-all duration-200 ease-in will-change-transform hover:-translate-y-3 hover:scale-105"
+  >
+    <div class="py-2">
+      <h2 class="title-mobile-h4 text-white">Project Name</h2>
+      <p class="text-white">Project description</p>
     </div>
-    <div class="grid-area self-end p-4">
-      <h1 class="title-mobile-h4 text-white">Project:</h1>
-      <p class="text-white">Project Name</p>
-      <p class="text-white">Technologies used:</p>
+    <div class="p-4">
+      <img
+        :src="props.image"
+        loading="lazy"
+        class="aspect-square h-auto w-full rounded-xl object-cover lg:aspect-video"
+      />
+    </div>
+    <div class="flex justify-between py-2">
+      <p class="text-white">Technologies used</p>
+      <div class="inline-flex">
+        <a class="text-white">GitHub</a>
+        <a class="text-white">Check Website</a>
+      </div>
     </div>
   </div>
 </template>
--- src/layout/Projects.vue
+++ src/layout/Projects.vue
@@ -1,7 +1,11 @@
 <script setup>
 import "vue3-carousel/carousel.css";
 import Card from "../components/Card.vue";
-import { Carousel, Slide, Navigation } from "vue3-carousel";
+import {
+  Carousel,
+  Slide,
+  Navigation as CarouselNavigation,
+} from "vue3-carousel";
 
 const images = Array.from({ length: 10 }, (_, index) => ({
   id: index + 1,
@@ -9,16 +13,15 @@ const images = Array.from({ length: 10 }, (_, index) => ({
 }));
 
 const config = {
-  itemsToShow: 1.5,
-  gap: 10,
   autoplay: 5000,
   wrapAround: true,
   pauseAutoplayOnHover: true,
+  gap: 20,
 };
 </script>
 
 <template>
-  <section class="mt-100">
+  <section class="mt-100 mb-11">
     <div>
       <h2 class="text-secondary title-mobile-h2 lg:title-h2 lg:mb-14">
         Projects
@@ -47,7 +50,14 @@ const config = {
       </Slide>
 
       <template #addons>
-        <Navigation />
+        <CarouselNavigation>
+          <template #prev>
+            <span class="text-primary">←</span>
+          </template>
+          <template #next>
+            <span class="text-primary">→</span>
+          </template>
+        </CarouselNavigation>
       </template>
     </Carousel>
   </section>
