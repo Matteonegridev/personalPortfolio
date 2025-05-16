91cf85a75374b137cc5b8949ac13ebd32c0a110b
Matteo <matteonegridev@gmail.com>
2025-05-16 11:11:41 +0200

    hero animation

--- src/layout/Hero.vue
+++ src/layout/Hero.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import Button from "../components/Button.vue";
 import HeroBackground from "../utils/HeroBackground.vue";
-import { motion, useTime, useTransform } from "motion-v";
+import { easeOut, motion, useTime, useTransform } from "motion-v";
 
 // Button Effect:
 const time = useTime();
@@ -13,21 +13,85 @@ const rotateBg = useTransform(rotate, (r) => {
 const navTo = () => {
   window.open("mailto:matteonegridev@gmail.com");
 };
+
+// Title Effect:
+const titleName = ["Matteo", "Negri"];
+
+const parentTitle = {
+  closed: {},
+  open: {
+    transition: {
+      staggerChildren: 0.25,
+    },
+  },
+};
+
+const childTitle = {
+  open: {
+    y: 0,
+    opacity: 1,
+    bounce: 1,
+    transition: {
+      duration: 0.5,
+      ease: [0.11, 0.97, 0.91, 0.19],
+    },
+  },
+  closed: {
+    y: -20,
+    opacity: 0,
+    transition: {
+      duration: 0.7,
+      delay: 0.6,
+      easeOut,
+    },
+  },
+};
+
+// Subtitle Effect:
+const subtitleVariant = {
+  closed: {
+    opacity: 0,
+    transition: {
+      duration: 0.3,
+    },
+  },
+  open: {
+    opacity: 1,
+    transition: {
+      duration: 1,
+      delay: 0.8,
+    },
+  },
+};
 </script>
 
 <template>
   <section class="h-[100dvh]">
     <HeroBackground />
     <div class="flex h-full flex-col justify-center">
-      <h1 class="title-h1 mb-2 text-white lg:mb-6 lg:w-[8ch] lg:text-9xl">
-        Matteo Negri
-      </h1>
+      <motion.h1
+        :variants="parentTitle"
+        while-in-view="open"
+        initial="closed"
+        class="title-h1 mb-2 text-white lg:mb-6 lg:w-[8ch] lg:text-9xl"
+      >
+        <motion.span
+          class="inline-block"
+          :variants="childTitle"
+          v-for="name in titleName"
+          :key="name"
+          >{{ name }}</motion.span
+        >
+      </motion.h1>
 
-      <p
+      <motion.p
+        :variants="subtitleVariant"
+        initial="closed"
+        while-in-view="open"
         class="title-mobile-h3 lg:body-font-desktop mb-8 w-[13ch] text-pretty text-white lg:w-full"
       >
         Web Developer UX Designer & Videoscribe Specialist.
-      </p>
+      </motion.p>
 
       <div class="relative self-start lg:hidden">
         <Button
