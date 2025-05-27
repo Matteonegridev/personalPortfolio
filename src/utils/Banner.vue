e832a8c1e68fe763c774ec7917b19bbd6ff6b2e6
Matteo <matteonegridev@gmail.com>
2025-05-27 14:19:39 +0200

    new banner

--- src/utils/Banner.vue
+++ src/utils/Banner.vue
@@ -1,6 +1,17 @@
 <script setup lang="ts">
 import { motion } from "motion-v";
 
+const tech = ["React.js", "Vue.js", " Astro.js", "Tailwind", "Node.js", "Scss"];
+const duplicatedTech = [...tech, ...tech];
+const skills = [
+  "UI/UX Design",
+  "Web Development",
+  "Motion Design",
+  "Accessibility",
+  "Design Systems",
+];
+const duplicatedSkills = [...skills, ...skills];
+
 const animateTitle = {
   x: ["0%", "-50%"],
   transition: {
@@ -24,34 +35,33 @@ const animateTitleBelow = {
   <section
     class="-mx-[var(--margin-mobile)] mt-35 lg:-mx-[var(--margin-desktop)]"
   >
+    <!-- Upper Line -->
     <div class="bg-primary mb-4 max-w-full overflow-hidden">
       <motion.div :animate="animateTitle" class="flex w-max py-3 text-white">
-        <span class="title-mobile-h4 tracking-[15px] text-nowrap uppercase">
-          React.js &#x2022; Vue.js &#x2022; Astro.js &#x2022; Tailwind &#x2022;
-          Node.js &#x2022; Scss
-          <span class="inline-block w-8"></span>
-        </span>
-        <span class="title-mobile-h4 tracking-[15px] text-nowrap uppercase">
-          React.js &#x2022; Vue.js &#x2022; Astro.js &#x2022; Tailwind &#x2022;
-          Node.js &#x2022; Scss
-          <span class="inline-block w-8"></span>
+        <span
+          class="title-mobile-h4 tracking-[15px] text-nowrap uppercase"
+          v-for="(el, i) in duplicatedTech"
+          :key="el"
+          >{{ el }} &#x2022;<span v-if="i !== duplicatedTech.length - 1"
+            >&nbsp;</span
+          >
         </span>
       </motion.div>
     </div>
+    <!-- Below Line -->
     <div class="bg-primary max-w-full overflow-hidden">
       <motion.div
         :animate="animateTitleBelow"
         class="flex w-max py-3 text-white"
       >
-        <span class="title-mobile-h4 tracking-[15px] text-nowrap uppercase">
-          UI/UX Design &#x2022; Web Development &#x2022; Motion Design &#x2022;
-          Accessibility &#x2022; Design Systems
-          <span class="inline-block w-8"></span>
-        </span>
-        <span class="title-mobile-h4 tracking-[15px] text-nowrap uppercase">
-          UI/UX Design &#x2022; Web Development &#x2022; Motion Design &#x2022;
-          Accessibility &#x2022; Design Systems
-          <span class="inline-block w-8"></span>
+        <span
+          v-for="(el, i) in duplicatedSkills"
+          :key="el"
+          class="title-mobile-h4 flex-nowrap tracking-[15px] uppercase"
+        >
+          {{ el }} &#x2022;<span v-if="i !== duplicatedSkills.length - 1"
+            >&nbsp;</span
+          >
         </span>
       </motion.div>
     </div>
